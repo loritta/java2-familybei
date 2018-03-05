be29437bbb9babd17f931d2869dc1fd33365a662
Pham <1796137@johnabbottcollege.net>
2018-03-05 12:31:21 -0500

    dailyscrum

--- Java3TPHouseholdBIE_TheFinal/src/HelperClasses/Database.java
+++ Java3TPHouseholdBIE_TheFinal/src/HelperClasses/Database.java
@@ -22,136 +22,135 @@ import javax.swing.JOptionPane;
 
 public class Database {
 
-  //for Larisa
-  /*private final static String HOSTNAME = "den1.mysql6.gear.host";
+    //for Larisa
+    /*private final static String HOSTNAME = "den1.mysql6.gear.host";
   private final static String DBNAME = "myjac";
   private final static String USERNAME = "myjac";
   private final static String PASSWORD = "Yt6wOA_!6byy";*/
-  public static final String DATE_FORMAT_SQL = "yyyy/MM/dd";
-  //for Tung
-  private final static String HOSTNAME = "den1.mysql6.gear.host";
-  private final static String DBNAME = "familybei";
-  private final static String USERNAME = "familybei";
-  private final static String PASSWORD = "tp%ipd12";
-  //correct password tp%ipd12 removed one letter to test
-  //I changed it (Larisa 1March) can we somehow go around the connection to 
-  //privatewhen an object s created from database?
-  private Connection conn = null;
-  private ResultSet rs = null;
-  private PreparedStatement pst = null;
+    public static final String DATE_FORMAT_SQL = "yyyy/MM/dd";
+    //for Tung
+    private final static String HOSTNAME = "den1.mysql6.gear.host";
+    private final static String DBNAME = "familybei";
+    private final static String USERNAME = "familybei";
+    private final static String PASSWORD = "tp%ipd12";
+    //correct password tp%ipd12 removed one letter to test
+    //I changed it (Larisa 1March) can we somehow go around the connection to 
+    //privatewhen an object s created from database?
+    private Connection conn = null;
+    private ResultSet rs = null;
+    private PreparedStatement pst = null;
 
-  public Database() {
-    try {
-      conn = DriverManager.getConnection(
-              "jdbc:mysql://" + HOSTNAME + "/" + DBNAME,
-              USERNAME, PASSWORD);
-    } catch (SQLException ex) {
-      JOptionPane.showMessageDialog(null,
-              "Error connecting to database: " + ex.getMessage(),
-              "Database error",
-              JOptionPane.ERROR_MESSAGE);
-      System.exit(0);
-    }
-  }
-
-  //why didn't you do it in the constructor???
-  public Connection connect() {
-
-    try {
-      conn = DriverManager.getConnection(
-              "jdbc:mysql://" + HOSTNAME + "/" + DBNAME,
-              USERNAME, PASSWORD);
-    } catch (SQLException e) {
-      //need to verify if null works
-      JOptionPane.showMessageDialog(null,
-              "Error connecting to database: " + e.getMessage(),
-              "Database error",
-              JOptionPane.ERROR_MESSAGE);
-      System.out.println("HelperClasses.Database.connect() - failed");
-      System.exit(0); // can't continue if database connection failed
-    }
-    return conn;
-  }
-
-  public String loginVerif(String password, String username) {
-    String sql = "select * from users where name = ? and password = ?";
-    String msg = "";
-    try {
-      pst = conn.prepareStatement(sql);
-      pst.setString(1, username);
-      pst.setString(2, password);
-      rs = pst.executeQuery();
-      if (rs.next()) {
-        msg = "success";
-      } else {
-        JOptionPane.showMessageDialog(null, "Invalid username and password");
-      }
-    } catch (SQLException | NullPointerException e) {
-      JOptionPane.showMessageDialog(null,
-              "Error connecting to database: " + e.getMessage(),
-              "Database error",
-              JOptionPane.ERROR_MESSAGE);
-    }
-    return msg;
-  }
-
-  //to work on the SQL phrasing here
-  public User createUserObject(String password, String username) throws SQLException {
-    String sql = "SELECT id, dob, familyid FROM users WHERE name = '" + username + "' and password ='" + password + "'";
-    int id = 0;
-    Date dob = null;
-    int familyId = 0;
-
-    try (Statement stmt = conn.createStatement();
-            ResultSet result = (ResultSet) stmt.executeQuery(sql)) {
-      while (result.next()) {
-        id = result.getInt("id");
-        dob = result.getDate("dob");
-        familyId = result.getInt("familyid");
-        System.out.println(familyId);
-      }
-    } catch (SQLException ex) {
-      System.out.println(ex.getMessage());
+    public Database() {
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://" + HOSTNAME + "/" + DBNAME,
+                    USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,
+                    "Error connecting to database: " + ex.getMessage(),
+                    "Database error",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
     }
 
-    User user = new User(id, username, password, dob, familyId);
+    //why didn't you do it in the constructor???
+    public Connection connect() {
 
-    return user;
-  }
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://" + HOSTNAME + "/" + DBNAME,
+                    USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            //need to verify if null works
+            JOptionPane.showMessageDialog(null,
+                    "Error connecting to database: " + e.getMessage(),
+                    "Database error",
+                    JOptionPane.ERROR_MESSAGE);
+            System.out.println("HelperClasses.Database.connect() - failed");
+            System.exit(0); // can't continue if database connection failed
+        }
+        return conn;
+    }
 
-  public static Timestamp nowSQL() {
-    Calendar cal = Calendar.getInstance();
-    java.util.Date now = cal.getTime();
-    java.sql.Timestamp currentTimestamp = new java.sql.Timestamp(now.getTime());
-    return currentTimestamp;
-  }
+    public String loginVerif(String password, String username) {
+        String sql = "select * from users where name = ? and password = ?";
+        String msg = "";
+        try {
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, username);
+            pst.setString(2, password);
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                msg = "success";
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid username and password");
+            }
+        } catch (SQLException | NullPointerException e) {
+            JOptionPane.showMessageDialog(null,
+                    "Error connecting to database: " + e.getMessage(),
+                    "Database error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+        return msg;
+    }
 
-  public Date strToDate(String str) {
-    SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_SQL);
-    Date sqlDate = new Date(Calendar.getInstance().getTime().getTime());
-    try {
-      java.util.Date utilDate = sdf.parse(str);
-      sqlDate = new Date(utilDate.getTime());
-    } catch (ParseException ex) {
-      System.out.println(ex.getMessage());
+    //to work on the SQL phrasing here
+    public User createUserObject(String password, String username) throws SQLException {
+        String sql = "SELECT id, dob, familyid FROM users WHERE name = '" + username + "' and password ='" + password + "'";
+        int id = 0;
+        Date dob = null;
+        int familyId = 0;
+
+        try (Statement stmt = conn.createStatement();
+                ResultSet result = (ResultSet) stmt.executeQuery(sql)) {
+            while (result.next()) {
+                id = result.getInt("id");
+                dob = result.getDate("dob");
+                familyId = result.getInt("familyid");
+                System.out.println(familyId);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        User user = new User(id, username, password, dob, familyId);
+
+        return user;
     }
-    return sqlDate;
-  }
-  public String transactionHistoryAvailable(int id) throws SQLException {
-        String sql = "SELECT * FROM transactions WHERE id='"+id+"' and Month(transdate)=MONTH(NOW())";
-    String msg="";
-    try (Statement stmt = conn.createStatement();
-          ResultSet result = stmt.executeQuery(sql)) {
-      Timestamp transDate;
-      while (result.next()) {
-        msg="empty";
-      }
+
+    public static Timestamp nowSQL() {
+        Calendar cal = Calendar.getInstance();
+        java.util.Date now = cal.getTime();
+        java.sql.Timestamp currentTimestamp = new java.sql.Timestamp(now.getTime());
+        return currentTimestamp;
     }
-    catch (SQLException ex) {
-      System.out.println(ex.getMessage());
+
+    public Date strToDate(String str) {
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_SQL);
+        Date sqlDate = new Date(Calendar.getInstance().getTime().getTime());
+        try {
+            java.util.Date utilDate = sdf.parse(str);
+            sqlDate = new Date(utilDate.getTime());
+        } catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return sqlDate;
     }
-    return msg;
+
+    public String transactionHistoryAvailable(int id) throws SQLException {
+        String sql = "SELECT * FROM transactions WHERE id='" + id + "' and Month(transdate)=MONTH(NOW())";
+        String msg = "";
+        try (Statement stmt = conn.createStatement();
+                ResultSet result = stmt.executeQuery(sql)) {
+            Timestamp transDate;
+            while (result.next()) {
+                msg = "empty";
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return msg;
     }
-  
- 
+
 }
